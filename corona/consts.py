5eb636ce05c0eb3de5a2f1d10233ee8f046760a1
antista <33634602+antista@users.noreply.github.com>
2020-09-04 15:19:16 +0500

    Typo refactored

--- corona/consts.py
+++ corona/consts.py
@@ -1,7 +1,7 @@
-# get_trend COVID-19 disease incubation period
+# COVID-19 disease incubation period
 INCUBATION = 14
 
-# get_trend COVID-19 disease duration according to Rospotrebnadzor
+# COVID-19 disease duration according to Rospotrebnadzor
 DURATION = 25
 
 # count of people in the world
